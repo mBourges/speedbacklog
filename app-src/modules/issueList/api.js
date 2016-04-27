9c3a9ddbe5e932b299d5275f28950f42fb9dd993
Matthieu Bourges <m.bourges@gmail.com>
2016-04-27 09:45:48 +0000

    fix(ISSUE LIST): Corrected api name

--- app-src/modules/issueList/api.js
+++ app-src/modules/issueList/api.js
@@ -2,5 +2,5 @@ import axios from 'axios';
 import { API_URL } from '../../app/constants';
 
 export function getIssues() {
-    return axios.get(API_URL + 'issues');
+    return axios.get(API_URL + 'issue');
 }
